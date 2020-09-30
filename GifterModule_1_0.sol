857cc292f858edc42575bf0196c5fa4eada57405
maxxflyer <maxxflyer@gmail.com>
2020-09-30 13:17:01 +0200

    Create GifterModule_1_0.sol

---
+++ GifterModule_1_0.sol
@@ -0,0 +1,72 @@
+/* 
+ *  GifterModule
+ *  VERSION: 1.0
+ *
+ */
+
+pragma solidity ^0.6.0;
+
+
+contract ERC20{
+    function allowance(address owner, address spender) external view returns (uint256){}
+    function transfer(address recipient, uint256 amount) external returns (bool){}
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool){}
+    function balanceOf(address account) external view returns (uint256){}
+}
+
+
+contract ItemsGifterDB{
+    
+    event Gifted(address gifted);
+    
+    address[] public modules_list;
+    mapping(address => bool)public modules;
+    
+    ERC20 public token;
+    address master;
+    address public receiver;
+    
+    constructor() public{
+        master=msg.sender;
+    }
+    
+    function gift(address tkn,uint amount,address gifted) public returns(bool){
+        require(modules[msg.sender]);
+        ERC20 token=ERC20(tkn);
+        require(token.transfer(gifted, amount));
+        emit Gifted(gifted);
+        return true;
+    } 
+    
+    function burn(address tkn)public returns(bool){
+        require(msg.sender==master);
+        ERC20 token=ERC20(tkn);
+        token.transfer(master, token.balanceOf(address(this)));
+    }
+    
+    function setModule(address new_module,bool set)public returns(bool){
+        require(msg.sender==master);
+        modules[new_module]=set;
+        if(set)modules_list.push(new_module);
+        return true;
+    }
+    
+    function setMaster(address new_master)public returns(bool){
+        require(msg.sender==master);
+        master=new_master;
+        return true;
+    }
+    
+}
+
+
+contract GifterModule {
+    
+    ItemsGifterDB public gifter=ItemsGifterDB(0xC9746af16e5d5cc414eDF53f91cBA76e6Eaf739D);
+    
+    function buy() payable public returns(bool){
+        require(gifter.gift(0x801F90f81786dC72B4b9d51Ab613fbe99e5E4cCD,1,msg.sender));
+        return true;
+    } 
+    
+}
